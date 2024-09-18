75c10a18266eb7f4795108dca97a03691309d01f
JmillerAcc <125284127+JmillerAcc@users.noreply.github.com>
2024-09-18 15:09:25 -0500

    fixed navbar issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { links } from "@/data/links";
+import { navItems } from "@/data";
 import { Hero } from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { About } from "@/components/About";
@@ -10,7 +10,7 @@ import { About } from "@/components/About";
 export default function Home(){
   return (
     <div className="flex h-screen px-10">
-      <FloatingNav navItems={links} />
+     <FloatingNav navItems={navItems}/>
       <div className="flex-col md:flex-row max-w-7xl justify-center items-center">
         <div className="w-[90vw] md:ml-[40px]">
           <Hero />
